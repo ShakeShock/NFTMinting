330fd01a709f59a260edc0b8b0bbac3b1437c8f7
rodrigo-pino <rodrodpino@gmail.com>
2022-03-09 14:28:03 -0500

    Mayor bug fixing and refactoring
    Change internal working to be ERC721 compatible

--- contracts/Shakers.sol
+++ contracts/Shakers.sol
@@ -1,14 +1,17 @@
 // SPDX-License-Identifier: MIT
 pragma solidity ^0.8.0;
 
-import "@openzeppelin/contracts/token/ERC721/ERC721.sol";
-import "@openzeppelin/contracts/access/Ownable.sol";
-import "hardhat/console.sol";
+ import "@openzeppelin/contracts/token/ERC721/ERC721.sol";
+ import "@openzeppelin/contracts/access/Ownable.sol";
+// import "hardhat/console.sol";
+import "./tools/StringTools.sol";
 
 contract Shaker is ERC721, Ownable {
-    uint256 constant MAX_SHAKERS = 1;
-    uint256 ids = 0;
+    using StringTools for string;
 
+    uint256 constant MAX_SHAKERS = 1;
+
+    uint256 ids = 0;
     uint32 public type1Free;
     uint32 public type2Free;
     uint32 public type3Free;
@@ -17,84 +20,83 @@ contract Shaker is ERC721, Ownable {
     struct Shaker {
         uint16 level;
         uint8 civilization;
-        uint8 stage;
+        uint8 class;
     }
 
-    mapping(address => Shaker) private _shakers;
-    mapping(address => uint32) private _avatars;
+    // Token ID to Shaker
+    mapping(uint => Shaker) private _shakers;
+    // Shaker Properties Hast to URI
     mapping(bytes32 => string) private _metadata;
 
     constructor(
         uint16[] memory _level,
         uint8[] memory _civilization,
-        uint8[] memory _stage,
+        uint8[] memory _class,
         string[] memory _links,
         uint32 maxFreeMinting
     )  ERC721("Shaker", "SKR") {
-        // Set IPFS location for each shaker type
+        // Set free minting
         type1Free = maxFreeMinting * 70 / 100;
         type2Free = maxFreeMinting * 20 / 100;
         type3Free = maxFreeMinting * 10 / 100;
 
+        // Set IPFS location for each shaker type
         for (uint i = 0; i < _level.length; i++){
-            bytes32 shakerHash = getShakerHash(_level[i], _civilization[i], _stage[i]);
+            bytes32 shakerHash = getShakerHash(_level[i], _civilization[i], _class[i]);
             _metadata[shakerHash] = _links[i];
         }
     }
 
     function mintShaker(uint32 shakerType) external payable {
-        require(shakerType <= 3 && shakerType >= 1, "Invalid shaker type");
+        require(shakerType <= 2, "Invalid shaker type");
 
         address sender = _msgSender();
-        console.log("Minting for ", sender);
-
         require(balanceOf(sender) < MAX_SHAKERS, "Already owns maximum number of shakers");         
 
-        _safeMint(sender, ids);
-
         if (msg.value == 0){
-            if (shakerType == 1){
+            if (shakerType == 0){
                 require(type1Free > 0, "No more type one free");
                 type1Free -= 1;
             }
-            if (shakerType == 2){
+            if (shakerType == 1){
                 require(type2Free > 0, "No more type two free");
                 type2Free -= 1;
             }
-            if (shakerType == 3){
+            if (shakerType == 2){
                 require(type3Free > 0, "No more type three free");
                 type3Free -= 1;
             }
         } else {
-            if (shakerType == 1)
+            if (shakerType == 0)
                 require(msg.value > 0.015 ether, "Insuficient funds");
-            if (shakerType == 2)
+            if (shakerType == 1)
                 require(msg.value > 0.03 ether, "Insuficient funds");
-            if (shakerType == 3)
+            if (shakerType == 2)
                 require(msg.value > 0.06 ether, "Insuficient funds");
         }
-        _shakers[sender] = primitiveShaker();
-        _avatars[sender] = shakerType;
+
+        _safeMint(sender, ids);
+        _shakers[ids] = primitiveShaker();
+
         ids += 1;
     }
 
-    function tokenURI() public view returns (string memory) {
+    function tokenURI(uint id) public view virtual override returns (string memory) {
         address sender = _msgSender();
-        require(balanceOf(sender) == 1, "Player does not own a Shaker");
+        require(balanceOf(sender) > 0, "Player does not own a Shaker");
 
-        Shaker memory playerShaker = _shakers[sender];
-
+        Shaker memory playerShaker = _shakers[id];
         return string(abi.encodePacked(_baseURI(), getShakerMetadataLink(playerShaker)));
     }
 
     function getShakerMetadataLink(Shaker memory s) internal view returns (string memory) {
-        return getShakerMetadataLink(s.level, s.civilization, s.stage);
+        return getShakerMetadataLink(s.level, s.civilization, s.class);
     }
 
     function getShakerMetadataLink(
-        uint16 _level, uint8 _civilization, uint8 _stage
+        uint16 _level, uint8 _civilization, uint8 _class
     ) public view returns (string memory) {
-        bytes32 shakerHash = getShakerHash(_level, _civilization, _stage);
+        bytes32 shakerHash = getShakerHash(_level, _civilization, _class);
         
         // Need to verifiy if that combination does exists!
         require (
@@ -105,23 +107,28 @@ contract Shaker is ERC721, Ownable {
         return _metadata[shakerHash];
     }
 
-    function getShakerHash(
-        uint16 _level, uint8 _civilization, uint8 _stage
-    ) public pure returns(bytes32) {
-        return keccak256(abi.encodePacked(_level, _civilization, _stage));
-    }
+    function addShakerMetadataLink(
+        uint16 _level, uint8 _civilization, uint8 _class, string calldata _link
+    ) external onlyOwner validURI(_link){
 
-    function setShakerMetadataLink(
-        uint16 _level, uint8 _civilization, uint8 _stage, string calldata _link
-    ) external onlyOwner {
-        require(
-            keccak256(abi.encode(_link)) != keccak256(abi.encode("")),
-            "Link cannot be empty"
-        );
+        bytes32 shakerHash = getShakerHash(_level, _civilization, _class);
+        string memory uri = _metadata[shakerHash];
+         require(!uri.empty(), "There is already a link for this shaker");
 
-        bytes32 shakerHash = getShakerHash(_level, _civilization, _stage);
        _metadata[shakerHash] = _link;
     }
+
+    function overwriteShakerMetadataLink(
+        uint16 _level, uint8 _civilization, uint8 _class, string calldata _link
+    ) external onlyOwner validURI(_link) {
+
+        bytes32 shakerHash = getShakerHash(_level, _civilization, _class);
+        require(!_metadata[shakerHash].empty(), "There is no link to override for this shaker");
+
+       _metadata[shakerHash] = _link;
+    }
+
+
 
     // Increase shaker level, which means it has a new link in ipfs
     // If invalid level revert
@@ -136,15 +143,29 @@ contract Shaker is ERC721, Ownable {
     // If cannot further advance then revert
     function increaseShakerStage() private {}
 
+    // Set how much it is possible to free mint again
+    function setFreeMinting(uint32 shakerType) public onlyOwner {}
+
     function _baseURI() internal pure override returns (string memory) {
         return "ipfs://";
+    }
+
+    function getShakerHash(
+        uint16 _level, uint8 _civilization, uint8 _class
+    ) public pure returns(bytes32) {
+        return keccak256(abi.encodePacked(_level, _civilization, _class));
     }
 
     function primitiveShaker() public pure returns(Shaker memory){
         return Shaker({
             level: 0,
             civilization: 0,
-            stage: 0
+            class: 0
         });
+    }
+
+    modifier validURI(string memory _link) {
+        require(!_link.empty(), "Invalid URI");
+        _;
     }
 }
