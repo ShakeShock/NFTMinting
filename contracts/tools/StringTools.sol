02ecfa53164e920da5bf10d63987faff83c095fd
rodrigo-pino <rodrodpino@gmail.com>
2022-03-10 10:29:16 -0500

    Add pragma and licence to library

--- contracts/tools/StringTools.sol
+++ contracts/tools/StringTools.sol
@@ -1,3 +1,6 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.0;
+
 library StringTools {
     function equal(string memory s1, string memory s2) public pure returns (bool) {
         return (keccak256(abi.encode(s1)) == keccak256(abi.encode(s2)));
